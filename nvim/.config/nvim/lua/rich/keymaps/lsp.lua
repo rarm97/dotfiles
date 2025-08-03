76beee36f22a7609f856abc7a4c8c62cd48c0efe
Richard Armstrong <rarm97@pm.me>
2025-08-03 18:26:37 +1000

    Actually added the keymap rather than just the mapping.

--- nvim/.config/nvim/lua/rich/keymaps/lsp.lua
+++ nvim/.config/nvim/lua/rich/keymaps/lsp.lua
@@ -2,7 +2,7 @@ vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Line Diag
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev Diagnostic" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
 
-vim.keymap.set("n", "<leader>rf", vim.lsp.buf.definition, { desc = "Goto Definition" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename function and usages" })
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Goto Implementation" })
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Goto References" })
