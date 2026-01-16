dc2e3c991b502dd235b1c7cb702f111b03dcf330
Richard Armstrong <rarm97@pm.me>
2026-01-16 12:44:32 +1100

    Groundwork for Wezterm as default terminal.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,23 @@
-
 -- ~/.wezterm.lua
 local wezterm = require 'wezterm'
 
 return {
-  font_size = 14.0,
-  color_scheme = "Tokyo Night Storm",
-  enable_tab_bar = false,
-  window_padding = {
-    left = 4, right = 4, top = 4, bottom = 4,
-  },
-  use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
+    font_size = 14.0,
+    color_scheme = "Tokyo Night Storm",
+
+    use_fancy_tab_bar = false,
+    hide_tab_bar_if_only_one_tab = true,
+    enable_tab_bar = false,
+    window_padding = {
+        left = 4, right = 4, top = 4, bottom = 4,
+    },
+
+    enable_kitty_keyboard = true, 
+    set_environmental_variables = {
+        TERM = "xterm-256color",
+    }, 
+
+    default_prog = { "/bin/zsh", "-lc", "tmux new -A -s main" }, 
+
 }
 
